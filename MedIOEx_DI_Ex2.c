79528bd5e282ccf471980adb12b9f869b45a1ee2
Polly Electronics <yanikgo@itu.edu.tr>
2017-08-14 00:43:43 +0300

    Update MedIOEx_DI_Ex2.c
    some basic changes

--- MedIOEx_DI_Ex2.c
+++ MedIOEx_DI_Ex2.c
@@ -21,13 +21,13 @@ int main(){
     	arr_diGPIO[0] = pe2a_DI_getVal(pe2a_GPIO_J14_16); //DI : emergency stop NC
     	arr_diGPIO[1] = pe2a_DI_getVal(pe2a_GPIO_J14_15); //DI : emergency stop NC
 
-      arr_diGPIO[2] = pe2a_DI_getVal(pe2a_GPIO_J14_14); //DI : emergency stop NC
-      arr_diGPIO[3] = pe2a_DI_getVal(pe2a_GPIO_J14_13); //DI : emergency stop NC
+      	arr_diGPIO[2] = pe2a_DI_getVal(pe2a_GPIO_J14_14); //DI : emergency stop NC
+      	arr_diGPIO[3] = pe2a_DI_getVal(pe2a_GPIO_J14_13); //DI : emergency stop NC
 
 
             if(arr_diGPIO[0]  || arr_diGPIO[1] || arr_diGPIO[2] || arr_diGPIO[3] ){
 
-                printf("Emergency Status is NOT OK , buzzer should be off \n")
+                printf("Emergency Status is NOT OK , buzzer should be OFF \n")
                 pe2a_DO_setHigh(pe2a_GPIO_J4_1); //buzzer ON
                 pe2a_DO_setHigh(pe2a_GPIO_J4_2);
 
@@ -36,7 +36,7 @@ int main(){
             else
             {
 
-                printf("Emergency Status is OK , buzzer should be off \n")
+                printf("Emergency Status is OK , buzzer should be ON \n")
                 pe2a_DO_setLow(pe2a_GPIO_J4_1); //buzzer OFF
                 pe2a_DO_setLow(pe2a_GPIO_J4_2);
 
